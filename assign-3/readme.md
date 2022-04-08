2688d9315489556a72eb6f93cf7d075c1912096d
tanmay-rpatil <f20190054@goa.bits-pilani.ac.in>
2022-04-08 23:54:00 +0530

    server binds to first ip that's not localhost

--- assign-3/readme.md
+++ assign-3/readme.md
@@ -1,4 +1,4 @@
-# Compilation 
+# Compilation
 
 ```bash
 $ make
@@ -15,7 +15,9 @@ $ ./s 4444
 # format ./s [port no]
 ```
 
-## Client 
+## Client
+
+For each client, open a new terminal, and run:
 
 ```bash
 $ ./c 127.0.0.1 4444
@@ -24,11 +26,84 @@ $ ./c 127.0.0.1 4444
 ```
 
 # Replay demo
-For best results, please run when terminal is >= 83 characters wide.
-This will replay the demo in real time.
+
+For best results, please run when terminal is >= 93 characters wide. This will replay the demo in real time.
+The replay will show an execution of the server code for 1.5 minutes.
 
 ```bash
 $ scriptreplay --timing=script_log scriptfile
 ```
 
-***
+For the 6 instances of clients tested, execution is seen below:
+
+```
+❯ ./c 127.0.0.1 4444
+# Connection established! type msg after the prompt 'C>' 
+C>hello s
+Scanned: hello s
+C>quit
+Scanned: quit
+# exiting
+```
+
+```
+❯ ./c 127.0.0.1 4444
+# Connection established! type msg after the prompt 'C>' 
+C>quit this?
+Scanned: quit this?
+C>yes 
+Scanned: yes 
+C>quit
+Scanned: quit
+# exiting
+```
+
+```
+❯ ./c 127.0.0.1 4444
+# Connection established! type msg after the prompt 'C>' 
+C>new client
+Scanned: new client
+C>will someone quit?
+Scanned: will someone quit?
+C>quit
+Scanned: quit
+# exiting
+```
+
+```
+❯ ./c 127.0.0.1 4444
+# Connection established! type msg after the prompt 'C>' 
+C>quit
+Scanned: quit
+# exiting
+```
+
+```
+❯ ./c 127.0.0.1 4444
+# Connection established! type msg after the prompt 'C>' 
+C>last one in
+Scanned: last one in
+C>quit
+Scanned: quit
+# exiting
+```
+
+```
+❯ ./c 127.0.0.1 4444
+# Server refused connection
+❯ ./c 127.0.0.1 4444
+# Connection established! type msg after the prompt 'C>' 
+C>yay! can join now!
+Scanned: yay! can join now!
+C>bye
+Scanned: bye
+C>quit
+Scanned: quit
+# exiting
+```
+
+Screenshot -
+
+![](ss.png)
+
+---
--- assign-3/server.c
+++ assign-3/server.c
@@ -5,6 +5,8 @@
 //ip addr
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <linux/if_link.h>
 //memset
 #include <string.h> 
 
@@ -80,12 +82,14 @@ int main(int argc, char const *argv[])
 {
     client_count = 0;
 	struct sockaddr_in address;
-	int addrlen;
+	int addrlen,family,s;
 	sem_init(&sem,0,1);
 	struct hostent *hostnet_str;
 	struct in_addr localaddr;
+	struct ifaddrs *ifap; // for network interfaces
 	int PORT = 4444;
-	
+	char host[NI_MAXHOST];
+
 	if (argc >=2)//port
     {
         PORT = atoi(argv[1]);
@@ -94,33 +98,64 @@ int main(int argc, char const *argv[])
 	//close sockets when ctrl+c
 	signal(SIGINT, intHandler);
 
+	//get available interfaces
+	if (getifaddrs(&ifap)<0){
+		perror("[error] in getting network interfaces");
+		exit(EXIT_FAILURE);
+	}
+
+	struct ifaddrs *ll_node = ifap;
+	struct ifaddrs *old_node;
+	while( (ll_node) != NULL ){
+		family = ll_node->ifa_addr->sa_family;
+
+		if (family == AF_INET) {
+			printf("[info] Interface name: %s\n",ll_node->ifa_name);
+			s = getnameinfo(ll_node->ifa_addr,
+					sizeof(struct sockaddr_in),
+					host, NI_MAXHOST,
+					NULL, 0, NI_NUMERICHOST);
+			if (s != 0) {
+				printf("getnameinfo() failed: %s\n", gai_strerror(s));
+				exit(EXIT_FAILURE);
+			}
+
+			printf("\t\taddress: <%s>\n", host);
+			if( strcmp(ll_node->ifa_name, "lo") == 0 ){
+				printf("[info] ignoring loopback\n");
+			}
+			else{
+				break;
+			}
+		}
+		// old_node = ll_node;
+		ll_node = ll_node->ifa_next;
+	}
+
+
 	//create socket with options: domain: AF_INET for ipv4, type: SOCK_STREAM for TCP, protocol: 0 for IP.
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if(server_fd<0){
 		perror("-> failed to create socket\n");
 		exit(1);
 	}
-
-	address.sin_family = AF_INET; //IP
-	address.sin_addr.s_addr = INADDR_ANY; //local ip of the machine
+	printf("[info] using the ip: %s\n", host);
+	address.sin_family = AF_INET; //IPv4
+	if(inet_pton(AF_INET, host, &address.sin_addr) <= 0) 
+    {
+        printf("addess converesion error\n");
+        exit (1);
+    }
 	address.sin_port = htons( PORT ); //set port to 4444
+	inet_ntop(AF_INET, &(address.sin_addr), host, INET_ADDRSTRLEN);
+	printf("[info] attempt to bind to: %s\n", host);
 	//bind to a port and ip
 	if( bind( server_fd, (struct sockaddr *) &address, sizeof(address) ) < 0 ){
 		perror("-> failed to bind\n");
 		exit(2);//bind failed
 	}
+	printf("[success] bound to (ip:port) %s:%d\n",host,PORT);
 
-	char *hostname = malloc (MAXSIZE);
-	memset (hostname, 0, MAXSIZE);
-	gethostname (hostname, MAXSIZE);
-	hostnet_str = gethostbyname (hostname);
-	if(hostnet_str){
-		bcopy (*hostnet_str->h_addr_list++, (char *) &localaddr, sizeof (localaddr));
-		printf("-> bound to (ip:port) %s:%d\n",inet_ntoa(localaddr),PORT);
-	}
-	else{
-		printf("-> Error in getting hostname!");exit(EXIT_FAILURE);
-	}
 
 	//listen at a socket ->  accept connection requests, max qued connections set 1,
 	if( listen( server_fd, 3 ) < 0 ){
